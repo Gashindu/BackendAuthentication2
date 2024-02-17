ec042bbd48931c3b79982139baf82d54aaa0932e
Gashindu <gideonashindu2@gmai.com>
2024-02-17 09:53:45 -0800

    first commit

---
+++ src/main/java/net/goasoft/backendauthentication/BackendAuthentication2Application.java
@@ -0,0 +1,13 @@
+package net.goasoft.backendauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAuthentication2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAuthentication2Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/goasoft/backendauthentication/BackendAuthentication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.goasoft.backendauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAuthentication2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
